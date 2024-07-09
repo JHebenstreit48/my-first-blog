3445263a2f50a1f84da9aa27eba48da61d2de7dd
Justin <j.hebenstreit25@gmail.com>
2024-07-09 11:26:10 -0500

    added more JavaScript and some HTML

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,20 +5,8 @@ const backSelect = document.querySelectorAll('.back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
 
-// const blogSubmit = document.getElementById("submit")
-
-// let submitBtn = 'submit'
-
-// formEl.addEventListener('click', function () {
-//     document.getElementById('submit')
-
-    // if ('submit' === true )
-
-    // else
-// })
 
 // TODO: Create a function that handles the case where there are no blog posts to display
-
 // This should be an if else statment
 
 
@@ -33,11 +21,8 @@ function displayMessage() {
     // if else
 }
 
-
 // TODO: Call the `renderBlogList` function
 //either a return function on console.log function
-
-
 
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,28 @@
 // TODO: Create a variable that selects the form element
-const blogNameForm = document.querySelectorAll('form');
+const blogForm = document.querySelectorAll('form');
+let errorMessage = document.getElementById('error')
+let userField = document.getElementById('username');
+let titleField = document.getElementById('title');
+let contentField = document.getElementById('content');
+let formSubmit = document.getElementById('form')
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
 
-
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
-// This will be something similar to the following:
 
-// formSubmit.addEventListener('click', function () {
-//  
-//   })
+formSubmit.addEventListener('submit', function (e) {
+    e.preventDefault()
+    if (userField.value.trim() === "" || titleField.value.trim() === "" || contentField.value.trim() === "") {
+        errorMessage.textContent = "Please complete the form";
+
+    } else {
+        const blogCreate = {
+            userField: userField,
+            titleField: titleField,
+            contentField: contentField,
+        }
+        localStorage.setItem('blogCreate', JSON.stringify(blogCreate))
+        redirectPage()
+    }
+})
